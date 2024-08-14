8450c8c62776e29edfd34f003b0def1d5472417e
Lianna <liannanovitz@gmail.com>
2024-08-14 12:31:55 -0700

    remove token if invalid

--- quickstart.py
+++ quickstart.py
@@ -1,6 +1,7 @@
 import os.path
 import base64
 
+from google.auth.exceptions import RefreshError
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -28,7 +29,11 @@ def main():
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                os.remove("token.json")
+                creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
