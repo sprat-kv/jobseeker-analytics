54ed7b35f161e777ea6f8fe103b081b0407278d8
Lianna <liannanovitz@gmail.com>
2024-11-25 15:12:34 -0800

    add email utils test outline

---
+++ email_utils.py
@@ -0,0 +1,21 @@
+from test_data import SAMPLE_MESSAGE
+
+
+def test_get_email_subject_line():
+    pass
+
+
+def test_get_email_from_address():
+    pass
+
+
+def test_get_email_domain():
+    pass
+
+
+def test_get_company_name():
+    pass
+
+
+def test_get_email_received_at():
+    pass
