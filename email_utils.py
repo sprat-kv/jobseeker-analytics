c74f3a87284c2c4783fc98dfaf306bdb1829d62e
Lianna <liannanovitz@gmail.com>
2024-11-25 15:39:54 -0800

    add get subject line

--- email_utils.py
+++ email_utils.py
@@ -1,21 +1,15 @@
-from test_data import SAMPLE_MESSAGE
+def get_email_headers(msg):
+    email_data = msg.get("payload", None)
+    if email_data:
+        return email_data.get("headers", None)
+    return None
 
 
-def test_get_email_subject_line():
-    pass
-
-
-def test_get_email_from_address():
-    pass
-
-
-def test_get_email_domain():
-    pass
-
-
-def test_get_company_name():
-    pass
-
-
-def test_get_email_received_at():
-    pass
+def get_email_subject_line(msg):
+    email_headers = get_email_headers(msg)
+    if email_headers:
+        for header in email_headers:
+            key = header.get("name")  # convert to dict for O(1) lookup ?
+            if key == "Subject":
+                return header.get("value")
+    return ""
---
+++ tests/test_email_utils.py
@@ -0,0 +1,26 @@
+from tests.constants import SAMPLE_MESSAGE
+import email_utils
+
+
+def test_get_email_subject_line():
+    subject_line = email_utils.get_email_subject_line(SAMPLE_MESSAGE)
+    assert (
+        subject_line
+        == "Invitation from an unknown sender: Interview with TestCompanyName @ Thu May 2, 2024 11:00am - 12pm (PDT) (appuser@gmail.com)"
+    )
+
+
+def test_get_email_from_address():
+    pass
+
+
+def test_get_email_domain():
+    pass
+
+
+def test_get_company_name():
+    pass
+
+
+def test_get_email_received_at():
+    pass
