a5d63ccf72f44599c500d0607970bdc082dbb253
Lianna <liannanovitz@gmail.com>
2024-12-30 22:59:23 -0800

    wrap everything in try except 1 email tracked is better than 0 emails tracked

--- email_utils.py
+++ email_utils.py
@@ -125,24 +125,32 @@ def get_email_parts(msg):
 
 
 def get_email_subject_line(msg):
-    email_headers = get_email_headers(msg)
-    if email_headers:
-        for header in email_headers:
-            key = header.get("name")  # convert to dict for O(1) lookup ?
-            if key == "Subject":
-                return header.get("value")
+    try:
+        email_headers = get_email_headers(msg)
+        if email_headers:
+            for header in email_headers:
+                key = header.get("name")  # convert to dict for O(1) lookup ?
+                if key == "Subject":
+                    return header.get("value", "")
+    except Exception as e:
+        logger.error(f"Error getting email subject line: {e}")
     return ""
 
 
 def get_email_from_address(msg):
-    email_headers = get_email_headers(msg)
-    if email_headers:
-        for header in email_headers:
-            key = header.get("name")
-            if key == "From":
-                if "<" in header.get("value"):
-                    return header.get("value").split("<")[1].strip(">")
-                return header.get("value")
+    try:
+        email_headers = get_email_headers(msg)
+        if email_headers:
+            for header in email_headers:
+                if header.get("name") == "From":
+                    # if value enclosed in <> then extract email address
+                    # else return the value as is
+                    from_address = header.get("value")
+                    if "<" in from_address:
+                        return from_address.split("<")[1].split(">")[0]
+                    return from_address
+    except Exception as e:
+        logger.error(f"Error getting email from address: {e}")
     return ""
 
 
@@ -162,84 +170,102 @@ def get_received_at_timestamp(id, msg):
 
 
 def get_email_domain_from_address(email_address):
-    return email_address.split("@")[1]
+    return email_address.split("@")[1] if "@" in email_address else ""
 
 
-def clean_email(email_body):
+def clean_email(email_body: str) -> list:
     import spacy
     from spacy_cleaner import processing, Cleaner
 
-    model = spacy.load("en_core_web_sm")
-    pipeline = Cleaner(
-        model,
-        processing.remove_stopword_token,
-        processing.remove_punctuation_token,
-        processing.remove_number_token,
-    )
-    return pipeline.clean([email_body])
+    try:
+        model = spacy.load("en_core_web_sm")
+        pipeline = Cleaner(
+            model,
+            processing.remove_stopword_token,
+            processing.remove_punctuation_token,
+            processing.remove_number_token,
+        )
+        return pipeline.clean([email_body])
+    except Exception as e:
+        logger.error(f"Error cleaning email: {e}")
+    return []
 
 
 def get_word_frequency(cleaned_email):
-    word_dict = {}
-    for word in cleaned_email[0].split(" "):
-        if word not in word_dict:
-            word_dict[word] = 1
-        else:
-            word_dict[word] += 1
+    try:
+        word_dict = {}
+        for word in cleaned_email[0].split(" "):
+            if word not in word_dict:
+                word_dict[word] = 1
+            else:
+                word_dict[word] += 1
 
-    word_dict_sorted = sorted(word_dict.items(), key=lambda item: item[1], reverse=True)
-    return word_dict_sorted
+        word_dict_sorted = sorted(word_dict.items(), key=lambda item: item[1], reverse=True)
+        return word_dict_sorted
+    except Exception as e:
+        logger.error(f"Error getting word frequency: {e}")
+    return []
 
 
 def get_top_word_in_email_body(msg_id, msg):
     import base64
     from bs4 import BeautifulSoup
 
-    parts = get_email_parts(msg)
-    if parts:
-        for part in parts:
-            if part.get("mimeType") not in [
-                "text/plain",
-                "text/html",
-            ]:
-                continue
-            if part.get("mimeType") and part.get("mimeType") in [
-                "text/plain",
-                "text/html",
-            ]:
-                data = base64.urlsafe_b64decode(
-                    part.get("body", {}).get("data", {})
-                ).decode("utf-8")
-                # Parse the content with BeautifulSoup
-                soup = BeautifulSoup(data, "html.parser")
-                # Extract the plain text from the HTML content
-                email_text = soup.get_text()
-                cleaned_text = clean_email(email_text)
-                # write to file for debugging
-                # with open(f"data/{msg_id}.txt", "w") as f:
-                #     f.write(email_text)
-                # with open(f"data/{msg_id}_cleaned.txt", "w") as f:
-                #     f.write(cleaned_text[0])
-                if cleaned_text:
-                    word_frequency = get_word_frequency(cleaned_text)
-                    # with open(f"data/{msg_id}_word_frequency.txt", "w") as f:
-                    #     f.write(str(word_frequency))
-                    top_capitalized_word = get_top_consecutive_capitalized_words(
-                        word_frequency
-                    )
-                    return top_capitalized_word or cleaned_text[0][0]
+    try:
+        parts = get_email_parts(msg)
+        if parts:
+            for part in parts:
+                if part.get("mimeType") not in [
+                    "text/plain",
+                    "text/html",
+                ]:
+                    continue
+                if part.get("mimeType") and part.get("mimeType") in [
+                    "text/plain",
+                    "text/html",
+                ]:
+                    data = base64.urlsafe_b64decode(
+                        part.get("body", {}).get("data", {})
+                    ).decode("utf-8")
+                    # Parse the content with BeautifulSoup
+                    soup = BeautifulSoup(data, "html.parser")
+                    # Extract the plain text from the HTML content
+                    email_text = soup.get_text()
+                    cleaned_text = clean_email(email_text)
+                    # write to file for debugging
+                    # with open(f"data/{msg_id}.txt", "w") as f:
+                    #     f.write(email_text)
+                    # with open(f"data/{msg_id}_cleaned.txt", "w") as f:
+                    #     f.write(cleaned_text[0])
+                    if cleaned_text:
+                        word_frequency = get_word_frequency(cleaned_text)
+                        # with open(f"data/{msg_id}_word_frequency.txt", "w") as f:
+                        #     f.write(str(word_frequency))
+                        top_capitalized_word = get_top_consecutive_capitalized_words(
+                            word_frequency
+                        )
+                        if not top_capitalized_word:
+                            if len(cleaned_text) > 0:
+                                return cleaned_text[0]
+                        return top_capitalized_word or cleaned_text[0][0]
+    except Exception as e:
+        logger.error(f"Error getting top word: {e}")
     return ""
 
 
 def get_company_name(id, msg):
-    top_word = get_top_word_in_email_body(id, msg)
-    if not top_word:
-        # likely a calendar invite, haven't parsed these yet
-        # return email domain instead as shortcut
-        # TODO: compare email domain and top word to decide
-        from_address = get_email_from_address(msg)
-        return get_email_domain_from_address(from_address).split(".")[0]
-    return top_word
+    try:
+        top_word = get_top_word_in_email_body(id, msg)
+        if not top_word:
+            # likely a calendar invite, haven't parsed these yet
+            # return email domain instead as shortcut
+            # TODO: compare email domain and top word to decide
+            from_address = get_email_from_address(msg)
+            return get_email_domain_from_address(from_address).split(".")[0]
+        return top_word
+    except Exception as e:
+        logger.error(f"Error getting company name: {e}")
+    return ""
 
 
 def get_top_consecutive_capitalized_words(tuples_list):
@@ -247,20 +273,24 @@ def get_top_consecutive_capitalized_words(tuples_list):
     Helper function to parse company name from an email.
     We only want the top capitalized words that appear consecutively and with the same frequency.
     """
-    result = []
-    temp_group = []
-    max = float("-inf")
-    for i, (first, second) in enumerate(tuples_list):
-        is_capitalized = first and first[0].isupper()
+    try:
+        result = []
+        temp_group = []
+        max = float("-inf")
+        for i, (first, second) in enumerate(tuples_list):
+            is_capitalized = first and first[0].isupper()
 
-        if is_capitalized:
-            if not temp_group:
-                max = second
-                temp_group.append((first, second))
-            if temp_group and temp_group[-1][1] == second:
-                # Add to the current group if criteria match
-                temp_group.append((first, second))
-            if second < max:
-                break
-            result.append(first)
-    return " ".join(result)
+            if is_capitalized:
+                if not temp_group:
+                    max = second
+                    temp_group.append((first, second))
+                if temp_group and temp_group[-1][1] == second:
+                    # Add to the current group if criteria match
+                    temp_group.append((first, second))
+                if second < max:
+                    break
+                result.append(first)
+        return " ".join(result)
+    except Exception as e:
+        logger.error(f"Error getting top consecutive capitalized words: {e}")
+    return ""
