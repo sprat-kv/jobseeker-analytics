3c80bf8a0bf698f14e12654a605fad07ac77902f
Lianna <liannanovitz@gmail.com>
2024-12-31 09:06:26 -0800

    create authenticated user class

---
+++ auth_utils.py
@@ -0,0 +1,31 @@
+import os
+from google.oauth2.credentials import Credentials
+
+class AuthenticatedUser:
+    def __init__(self, creds: Credentials):
+        self.creds = creds
+        self.user_id = self.get_user_id()
+        self.filepath = self.get_user_filepath()
+
+    def get_user_id(self) -> str:
+        """
+        Retrieves the user ID from Google OAuth2 credentials.
+
+        Parameters:
+        - credentials: The Google OAuth2 credentials object.
+
+        Returns:
+        - user_id: The unique user ID.
+        """
+        user_info = self.creds.id_token
+        user_id = user_info['sub']  # 'sub' is the unique user ID
+        return user_id
+
+    def get_user_filepath(self) -> str:
+        """
+        Each user has their own directory to store their data.
+        """
+        return f"users/{self.user_id}/"
+
+        
+    
