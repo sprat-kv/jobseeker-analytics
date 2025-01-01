6e9d001fc83bba89f88d3b5b499e1ef4899ef2f7
Lianna <liannanovitz@gmail.com>
2024-12-31 21:20:56 -0800

    specify encoding with csv open (lint)

--- db_utils.py
+++ db_utils.py
@@ -13,19 +13,19 @@ def export_to_csv(user: AuthenticatedUser, message_data: dict):
     logger.info("user_id:%s exporting to CSV", user.user_id)
     if os.path.exists(main_filepath):  # TODO: dedupe records
         logger.info("user_id:%s appending to existing CSV file", user.user_id)
-        with open(main_filepath, "a") as f:
+        with open(main_filepath, "a", encoding="utf-8") as f:
             values = ",".join(
-                (
-                    f'"{str(message_data[key][0])}"'
-                    if "," in str(message_data[key][0])
-                    else str(message_data[key][0])
-                )
-                for key in message_data
+            (
+                f'"{str(message_data[key][0])}"'
+                if "," in str(message_data[key][0])
+                else str(message_data[key][0])
+            )
+            for key in message_data
             )
             f.write(values + "\n")
     else:
         logger.info("user_id:%s creating new CSV file", user.user_id)
-        with open(main_filepath, "w") as f:
+        with open(main_filepath, "w", encoding="utf-8") as f:
             headers = ",".join(message_data.keys())
             f.write(headers + "\n")
             values = ",".join(
