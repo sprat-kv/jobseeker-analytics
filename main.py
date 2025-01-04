4bce7e8948918ac2a7d96cc5acf4a77af0cbc2d6
Lianna <liannanovitz@gmail.com>
2025-01-03 20:28:04 -0800

    process all the emails now

--- main.py
+++ main.py
@@ -135,8 +135,6 @@ def fetch_emails(user: AuthenticatedUser) -> None:
 
         # Exporting the email data to a CSV file
         export_to_csv(user.filepath, user.user_id, message_data)
-        api_call_finished = True    # TODO: remove after debugging. only processes 1 email now
-        return
     api_call_finished = True
 
 # Define the route for OAuth2 flow
