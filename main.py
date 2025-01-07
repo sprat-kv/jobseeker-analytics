4c71a98391640d43de826aa1eb173443d41ac09e
Lianna <liannanovitz@gmail.com>
2025-01-06 19:46:46 -0800

    forgot brackets

--- main.py
+++ main.py
@@ -107,10 +107,10 @@ def fetch_emails(user: AuthenticatedUser) -> None:
                 logger.info("user_id:%s  successfully extracted email", user.user_id)
             else:
                 logger.info(f"user_id:%s failed to extract email", user.user_id)
-            message_data["company_name"] = result.get("company_name", "")
-            message_data["application_status"] = result.get("application_status", "")
-            message_data["received_at"] = result.get("received_at", "")
-            message_data["email_subject"] = result.get("subject_line", "")
+            message_data["company_name"] = [result.get("company_name", "")]
+            message_data["application_status"] = [result.get("application_status", "")]
+            message_data["received_at"] = [result.get("received_at", "")]
+            message_data["email_subject"] = [result.get("subject_line", "")]
             # Exporting the email data to a CSV file
             export_to_csv(user.filepath, user.user_id, message_data)
             api_call_finished = True    # TODO: reset indent after testing
