951aa03d4b0ef0041b19084bc652d30bc0e299dc
Lianna <liannanovitz@gmail.com>
2025-02-09 14:53:55 -0800

    add config unit tests

---
+++ backend/tests/test_config_utils.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+from utils.config_utils import get_settings
+
+@patch('utils.config_utils.config.Settings')
+def test_import_settings(mock_settings_call):
+    import backend.utils.llm_utils
+    assert mock_settings_call.called
+    import backend.utils.auth_utils
+    assert mock_settings_call.called
+    import main
+    assert mock_settings_call.called
+
+@patch('utils.config_utils.config.Settings')
+def test_get_settings_only_called_once_with_lru(mock_settings_call):
+    get_settings()
+    get_settings()
+    # Ensure the Settings constructor is called only once due to lru_cache
+    mock_settings_call.assert_called_once()
