844a7e9c07445e08d2bf9d861e4511bb92a7bae3
Jed Antalan <jediel2424@gmail.com>
2025-02-08 22:07:37 -0500

    fixes

--- frontend/app/testpage/page.tsx
+++ frontend/app/testpage/page.tsx
@@ -3,29 +3,30 @@
 import { useEffect, useState } from "react";
 
 export default function TestPage() {
-    const [data, setData] = useState<{ message: string } | null>(null);
+	const [data, setData] = useState<{ message: string } | null>(null);
 
-    async function fetchTestData() {
-        try {
-            const res = await fetch("http://127.0.0.1:8000/test", {
-                method: "GET",
-                credentials: "include", // Include credentials if needed
-            });
-            const result = await res.json();
+	async function fetchTestData() {
+		try {
+			const res = await fetch("http://127.0.0.1:8000/test", {
+				method: "GET",
+				credentials: "include" // Include credentials if needed
+			});
+			const result = await res.json();
+			
             setData(result);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
-    }
+		} catch (error) {
+			setData({ message: "Error fetching data: " + error });
+		}
+	}
 
-    useEffect(() => {
-        fetchTestData();
-    }, []);
+	useEffect(() => {
+		fetchTestData();
+	}, []);
 
-    return (
-        <div>
-            <h1>Date from FastAPI test endpoint:</h1>
-            {data ? <p>{data.message}</p> : <p>Loading...</p>}
-        </div>
-    );
+	return (
+		<div>
+			<h1 className="text-xl font-bold">Data from FastAPI test endpoint:</h1>
+			{data ? <p className="text-blue-500">{data.message}</p> : <p>Loading...</p>}
+		</div>
+	);
 }
