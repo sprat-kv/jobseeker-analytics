34a731cb15424b603f47ef79b893eb4d017fab09
Lianna <liannanovitz@gmail.com>
2025-02-09 15:28:32 -0800

    add dummy

---
+++ backend/tests/test_llm_utils.py
@@ -0,0 +1,7 @@
+# from unittest.mock import patch
+# from utils.config_utils import get_settings
+
+# @patch('utils.config_utils.config.Settings')
+# def test_llm_utils_import(mock_settings_call):
+#     import backend.utils.llm_utils
+#     assert mock_settings_call.called
