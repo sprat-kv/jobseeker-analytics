557b58ae5b3fa27b62fcd2434e087f182e3ebbe4
Mugdha Banthwan <113299553+m-banthwan@users.noreply.github.com>
2025-02-10 12:02:14 -0500

    Create company_jobs.py

---
+++ backend/db/company_jobs.py
@@ -0,0 +1,18 @@
+from sqlmodel import SQLModel, Field, UniqueConstraint
+from datetime import datetime
+
+class Company_jobs(SQLModel, table=True):
+    __tablename__ = 'company_jobs'
+    job_id: int = Field(default=None, primary_key=True)
+    company_id: int = Field(foreign_key="companies.company_id", nullable=False)
+    job_title_id: int = Field(foreign_key="job_titles.job_title_id", nullable=False)
+    job_description: str 
+    job_posted_at: datetime 
+    job_location: str
+
+    __table_args__ = (
+        # Ensure that company_name and company_email_domain together are unique
+        UniqueConstraint('company_id', 'job_title_id', 'job_location', name='non_unique_job_title_and_location'),
+    )
+
+
