ff608504aa2a07849c29a38d07ca0dd260dc59a4
Lianna <liannanovitz@gmail.com>
2025-02-09 20:46:15 -0800

    add json log

--- backend/tests/test_config_utils.py
+++ backend/tests/test_config_utils.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch
 from utils.config_utils import get_settings
-
+from config import Settings
+import pytest
+import json
 
 @patch("utils.config_utils.config.Settings")
 def test_get_settings_only_called_once_with_lru(mock_settings_call):
@@ -16,3 +18,27 @@ def test_import_settings_does_not_raise_error():
     import backend.utils.llm_utils  # noqa: F401
     import backend.utils.auth_utils  # noqa: F401
     import main  # noqa: F401
+
+
+def test_decode_scopes_valid_json():
+    input_str = '["https://www.googleapis.com/auth/gmail.readonly", "openid"]'
+    expected_output = ["https://www.googleapis.com/auth/gmail.readonly", "openid"]
+    assert Settings.decode_scopes(input_str) == expected_output
+
+
+def test_decode_scopes_with_extra_quotes():
+    input_str = '\'["https://www.googleapis.com/auth/gmail.readonly", "openid"]\''
+    expected_output = ["https://www.googleapis.com/auth/gmail.readonly", "openid"]
+    assert Settings.decode_scopes(input_str) == expected_output
+
+
+def test_decode_scopes_invalid_json():
+    input_str = '["https://www.googleapis.com/auth/gmail.readonly", "openid"'
+    with pytest.raises(json.JSONDecodeError):
+        Settings.decode_scopes(input_str)
+
+
+def test_decode_scopes_empty_string():
+    input_str = ''
+    with pytest.raises(json.JSONDecodeError):
+        Settings.decode_scopes(input_str)
