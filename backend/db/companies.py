821a5299a5be2d278cf701c1066e345d992de1bf
Mugdha Banthwan <113299553+m-banthwan@users.noreply.github.com>
2025-02-10 21:32:58 -0500

    Update companies.py
    Updated the code according to reviewer's comments

--- backend/db/companies.py
+++ backend/db/companies.py
@@ -5,12 +5,10 @@ class Companies(SQLModel, table=True):
     company_id: int = Field(default=None, primary_key=True)
     company_name: str
     company_email_domain: str 
-    company_location: str
-    company_age: int = Field(default=0)
 
     __table_args__ = (
         # Ensure that company_name and company_email_domain together are unique
-        UniqueConstraint('company_name', 'company_email_domain', name='non_unique_company_name_and_domain'),
+        UniqueConstraint('company_name', 'company_email_domain', name='unique_company_name_and_domain'),
     )
 
     
