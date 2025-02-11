d03a833f37935bfcda30f6020141babe5bd565b6
nlahlborg <69569569+nlahlborg@users.noreply.github.com>
2025-02-10 20:05:45 -0800

    add some items to the exclusion list

--- backend/tests/test_constants.py
+++ backend/tests/test_constants.py
@@ -199,7 +199,11 @@ DESIRED_FAIL_APPLIED_EMAIL_FILTER_SUBJECT_FROM_PAIRS = (
     ("watering applied to plants", "do-not-reply@wateringapp.net"), #made up, would be better to capture the real example
     ("your application for rental property was viewed", "no-reply@comet.zillow.com"),
     ("PR Portal (PR application) - Your immigration status update application has been successfully submitted",
-        "IRCC.DoNotReply-NePasRepondre.IRCC@prson-srpel.apps.cic.gc.ca")
+        "IRCC.DoNotReply-NePasRepondre.IRCC@prson-srpel.apps.cic.gc.ca"),
+    ("you have new application updates this week", "jobs-noreply@linkedin.com"),
+    ("Get career advice to guide you to success", "insights@careerseeker.accenture.com"),
+    ("farewell and thank you from", "personalemail@domain.com"),
+    ("Verify your candidate account", "accenture@myworkday.com")
 )
 
 SAMPLE_FILTER_PATH = Path(__file__).parent / "sample_base_filter.yaml"
