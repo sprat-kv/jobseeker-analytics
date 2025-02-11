e0aedc5ce9057fbcd89b82dceecabd11b6ee712a
Mugdha Banthwan <113299553+m-banthwan@users.noreply.github.com>
2025-02-10 21:45:00 -0500

    Update job_titles.py
    Changed the class name according to reviewer's comment

--- backend/db/job_titles.py
+++ backend/db/job_titles.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, Field, UniqueConstraint
 
-class Job_titles(SQLModel, table=True):
+class JobTitles(SQLModel, table=True):
     __tablename__ = 'job_titles'
     job_title_id: int = Field(default=None, primary_key=True)
     job_title: str 
