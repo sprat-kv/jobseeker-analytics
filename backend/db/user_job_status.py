071f670b89fe23215468173199009e684e655ddf
happynasit <105992603+happynasit@users.noreply.github.com>
2025-02-11 15:14:13 -0500

    Create User Job Status Model

---
+++ backend/db/user_job_status.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, Field
+
+class UserJobStatus(SQLModel, table=True):
+    __tablename__ = "user_job_status"
+    user_id: int = Field(foreign_key="users.user_id", nullable=False)
+    job_id: int = Field(foreign_key="company_jobs.job_id", nullable=False)
+    status_id: int = Field(foreign_key="job_status.status_id", nullable=False)
+    
