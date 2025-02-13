50895b14ee4a2b15faf5cbf7faae79116063498a
Jed Antalan <jediel2424@gmail.com>
2025-02-13 00:53:03 -0500

    success page success message

--- frontend/app/success/page.tsx
+++ frontend/app/success/page.tsx
@@ -1,3 +1,8 @@
 export default function SuccessPage() {
-	return <div>Success Page</div>;
+	return (
+		<div className="flex flex-col items-center justify-center text-center pt-64">
+			<h1 className="text-3xl font-bold text-green-500">Success! Your file is ready.</h1>
+			<p className="pt-8">Click the button below to download your file.</p>
+		</div>
+	);
 }
