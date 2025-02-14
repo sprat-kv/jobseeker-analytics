f33569751662ad2a22c55fa4c08deab5b26eb7a9
AxisShine <alicejsedgwick2004@gmail.com>
2025-02-14 12:19:33 -0500

    Added download button

--- frontend/app/success/page.tsx
+++ frontend/app/success/page.tsx
@@ -3,6 +3,13 @@ export default function SuccessPage() {
 		<div className="flex flex-col items-center justify-center text-center pt-64">
 			<h1 className="text-3xl font-bold text-green-500">Success! Your file is ready.</h1>
 			<p className="pt-8">Click the button below to download your file.</p>
+			<a
+				href="/path-to-csv-file-in-app" //TODO
+				download
+				className="mt-6 px-6 py-3 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 transition"
+			>
+				Download Job Application Data
+			</a>
 		</div>
 	);
 }
