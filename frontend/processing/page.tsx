3f90311b62214504c1cf31d8d3a27bcfc9a286d8
Mugdha Banthwan <113299553+m-banthwan@users.noreply.github.com>
2025-02-14 15:50:26 -0500

    Create page.tsx
    Processing page feature

---
+++ frontend/processing/page.tsx
@@ -0,0 +1,13 @@
+import Spinner from "../../components/spinner";
+
+const ProcessingPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-semibold mb-4">We are processing your job!</h1>
+            <Spinner />
+            <p className="text-lg mt-4">Your job is being processed. You will be redirected to the download page once it's ready.</p>
+        </div>
+    );
+};
+
+export default ProcessingPage;
