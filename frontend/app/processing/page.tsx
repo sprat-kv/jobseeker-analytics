99690ea2f071eca8122fba3abc4211ccaa437f0f
Jed Antalan <jediel2424@gmail.com>
2025-02-15 17:30:50 -0500

    lint fixes

--- frontend/app/processing/page.tsx
+++ frontend/app/processing/page.tsx
@@ -1,13 +1,15 @@
 import Spinner from "../../components/spinner";
 
 const ProcessingPage = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-3xl font-semibold mb-4">We are processing your job!</h1>
-            <Spinner />
-            <p className="text-lg mt-4">Your job is being processed. You will be redirected to the download page once it's ready.</p>
-        </div>
-    );
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1 className="text-3xl font-semibold mb-4">We are processing your job!</h1>
+			<Spinner />
+			<p className="text-lg mt-4">
+				Your job is being processed. You will be redirected to the download page once it&#39;s ready.
+			</p>
+		</div>
+	);
 };
 
 export default ProcessingPage;
