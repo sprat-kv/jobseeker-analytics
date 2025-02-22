4dfb56cf580afb9ae095ee8673076cd302b555ce
Elyse Ando <elysekando@gmail.com>
2025-02-22 15:19:38 -0500

    Ran format and lint.

--- frontend/tests/homepage.test.ts
+++ frontend/tests/homepage.test.ts
@@ -4,7 +4,10 @@ import { test, expect } from "@playwright/test";
 test("Google Login button is visible", async ({ page }) => {
 	await page.goto("http://localhost:3000");
 
-	const button = page.locator('div').filter({hasText: /^Login with Google$/}).getByRole('button');
+	const button = page
+		.locator("div")
+		.filter({ hasText: /^Login with Google$/ })
+		.getByRole("button");
 	await expect(button).toBeVisible();
 });
 
