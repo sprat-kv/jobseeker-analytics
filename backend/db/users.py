481c01d51ec4f5fdf8c94751f0083c2b43faebde
Justin <nitsujil3002@gmail.com>
2025-03-02 14:33:57 -0500

    google openid identifier

--- backend/db/users.py
+++ backend/db/users.py
@@ -3,5 +3,6 @@ from sqlmodel import SQLModel, Field
 class Users(SQLModel, table=True):
     __tablename__ = "users"
     user_id: int = Field(default = None, primary_key = True)
-    user_email: str = Field(nullable=False)                      # nullable=False - shouldn't be null
-    user_sheet_url: str = Field(nullable=False, default="")      # Ensures URL is not null
+    user_email: str = Field(nullable=False)                      
+    user_sheet_url: str = Field(nullable=False, default="") # Ensures URL is not null
+    google_openid: str = Field(nullable=False, unique=True) # OpenID identifier
