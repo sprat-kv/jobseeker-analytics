9486b63d1e77dad6969540a24da6aa30d45b8c69
Mugdha Banthwan <113299553+m-banthwan@users.noreply.github.com>
2025-03-02 13:52:14 -0500

    Update job_status.py
    Updated the name of the table.

--- backend/db/job_status.py
+++ backend/db/job_status.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
 
 class JobStatus(SQLModel, table=True):
-    __tablename__ = 'job_status'
+    __tablename__ = 'job_statuses'
     status_id: int = Field(default=None, primary_key=True)
     status_name: str 
     status_description: str
