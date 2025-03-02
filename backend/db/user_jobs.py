8799d12eb01fa71b1d8fd629c38ecbc207110b8f
happynasit <105992603+happynasit@users.noreply.github.com>
2025-03-02 14:39:55 -0500

    Changes: added primary key user_job_id

--- backend/db/user_jobs.py
+++ backend/db/user_jobs.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 class UserJobs(SQLModel, table=True):
     __tablename__ = "user_jobs"
+    user_job_id: int = Field(primary_key=True)
     user_id: int = Field(foreign_key="users.user_id", nullable=False)
     job_id: int = Field(foreign_key="company_jobs.job_id", nullable=False)
     applied_at: datetime
