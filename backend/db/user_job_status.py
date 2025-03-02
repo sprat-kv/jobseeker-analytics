de3bc3dfa1702e74828501d27b4a5672078940ab
happynasit <105992603+happynasit@users.noreply.github.com>
2025-03-02 14:21:21 -0500

    Changed from UserJobStatus to UserJobStatuses

--- backend/db/user_job_status.py
+++ backend/db/user_job_status.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, Field
 
-class UserJobStatus(SQLModel, table=True):
+class UserJobStatuses(SQLModel, table=True):
     __tablename__ = "user_job_status"
     user_id: int = Field(foreign_key="users.user_id", nullable=False)
     job_id: int = Field(foreign_key="company_jobs.job_id", nullable=False)
