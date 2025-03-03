26f6c71c1d62ab7ece1361ee65ecb01f1f2325f9
Lianna <32498202+lnovitz@users.noreply.github.com>
2025-03-03 13:47:49 -0800

    Update users.py

--- backend/db/users.py
+++ backend/db/users.py
@@ -4,5 +4,4 @@ class Users(SQLModel, table=True):
     __tablename__ = "users"
     user_id: int = Field(default = None, primary_key = True)
     user_email: str = Field(nullable=False)                      
-    user_sheet_url: str = Field(nullable=False, default="") # Ensures URL is not null
     google_openid: str = Field(nullable=False, unique=True) # OpenID identifier
