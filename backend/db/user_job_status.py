b1d0ac4b38359affe5a257e42a780e026f91e3e8
Justin <nitsujil3002@gmail.com>
2025-03-02 19:46:49 -0500

    foreign key and job_status reference

--- backend/db/user_job_status.py
+++ backend/db/user_job_status.py
@@ -2,7 +2,8 @@ from sqlmodel import SQLModel, Field
 
 class UserJobStatuses(SQLModel, table=True):
     __tablename__ = "user_job_status"
+    user_job_status_id: int = Field(default=None, primary_key=True) 
     user_id: int = Field(foreign_key="users.user_id", nullable=False)
     job_id: int = Field(foreign_key="company_jobs.job_id", nullable=False)
-    status_id: int = Field(foreign_key="job_status.status_id", nullable=False)
+    status_id: int = Field(foreign_key="job_statuses.status_id", nullable=False)
     
