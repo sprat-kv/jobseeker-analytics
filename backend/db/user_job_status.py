9d4ad1bb1203d5888fb31b37adfbf06f4bb1c023
Lianna <32498202+lnovitz@users.noreply.github.com>
2025-03-04 16:51:21 -0800

    Update user_job_status.py

--- backend/db/user_job_status.py
+++ backend/db/user_job_status.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
 
 class UserJobStatuses(SQLModel, table=True):
-    __tablename__ = "user_job_status"
+    __tablename__ = "user_job_statuses"
     user_job_status_id: int = Field(default=None, primary_key=True) 
     user_id: int = Field(foreign_key="users.user_id", nullable=False)
     job_id: int = Field(foreign_key="company_jobs.job_id", nullable=False)
