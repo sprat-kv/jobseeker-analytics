9b8093a3de090ad31a83b109bca3b2f290bec8bc
Justin <nitsujil3002@gmail.com>
2025-03-06 17:03:27 -0500

    modified add_user_email to be a create_user_email function in order to batch insert during fetch_emails_to_db

--- backend/db/utils/user_email_utils.py
+++ backend/db/utils/user_email_utils.py
@@ -1,4 +1,3 @@
-from sqlmodel import Session
 from db.user_email import UserEmail
 from datetime import datetime, timezone
 import email.utils
@@ -17,22 +16,18 @@ def parse_email_date(date_str: str) -> datetime:
     return dt
 
 
-def add_user_email(user, message_data: dict, session: Session) -> None:
+def create_user_email(user, message_data: dict) -> UserEmail:
     """
-    Insert email record into the user_emails table.
+    Creates a UserEmail record instance from the provided data.
     """
     received_at_str = message_data["received_at"][0]
     received_at = parse_email_date(received_at_str) # parse_email_date function was created as different date formats were being pulled from the data
     
-    email_record = UserEmail(
-        user_id=user.user_id,
+    return UserEmail(
+        user_id=user.user_id,  
         company_name=message_data["company_name"][0],
         application_status=message_data["application_status"][0],
         received_at=received_at,
         subject=message_data["subject"][0],
         email_from=message_data["from"][0]
     )
-    session.add(email_record)
-    session.commit()
-    session.refresh(email_record)
-    logger.info(f"Added email record for user {user.user_id}")
