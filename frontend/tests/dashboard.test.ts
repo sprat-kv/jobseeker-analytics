0bab16c1a79889e771a4b7ba408dab1f8e62a629
Elyse Ando <elysekando@gmail.com>
2025-03-07 23:00:33 -0500

    Commented out tests for old dashboard, wrote test for new dashboard.

--- frontend/tests/dashboard.test.ts
+++ frontend/tests/dashboard.test.ts
@@ -8,33 +8,42 @@ test.describe("Dashboard", () => {
 		await page.goto(DASHBOARD_URL);
 	});
 
-	test("should display title Your Job Application Data", async ({ page }) => {
-		await expect(page.getByRole("heading", { name: /Your Job Application Data/i })).toBeVisible();
+	test("should display title Job Applications Dashboard", async ({ page }) => {
+		await expect(page.getByRole("heading", { name: /Job Applications Dashboard/i })).toBeVisible();
 	});
 
-	test("should display Start Date dropdown", async ({ page }) => {
-		const startDateDropdown = await page.locator('[data-testid="start-date"]');
-		await expect(startDateDropdown).toBeVisible();
-	});
+	// test("should display Start Date dropdown", async ({ page }) => {
+	// 	const startDateDropdown = await page.locator('[data-testid="start-date"]');
+	// 	await expect(startDateDropdown).toBeVisible();
+	// });
 
 	// test("should display Sync New Data button", async ({ page }) => {
 	// 	const syncButton = await page.locator('[data-testid="sync-new-data"]');
 	// 	await expect(syncButton).toBeVisible();
 	// });
 
-	test("should display Download CSV button", async ({ page }) => {
-		const downloadButton = await page.locator('[data-testid="download-csv"]');
-		await expect(downloadButton).toBeVisible();
+	// test("should display Download CSV button", async ({ page }) => {
+	// 	const downloadButton = await page.locator('[data-testid="download-csv"]');
+	// 	await expect(downloadButton).toBeVisible();
+	// });
+
+	// test("should have correct download link", async ({ page }) => {
+	// 	const downloadButton = await page.locator('[data-testid="download-csv"]');
+	// 	const downloadHref = await downloadButton.getAttribute("href");
+	// 	expect(downloadHref).toBe(`${EXPECTED_DOWNLOAD_URL}`);
+	// });
+
+	// test("should display table", async ({ page }) => {
+	// 	const jobsTable = await page.locator('[data-testid="jobs-table"]');
+	// 	await expect(jobsTable).toBeVisible();
+	// });
+
+	test("should display failed to load applications", async ({ page }) => {
+		await expect(page.getByText("Failed to load applications")).toBeVisible();
 	});
 
-	test("should have correct download link", async ({ page }) => {
-		const downloadButton = await page.locator('[data-testid="download-csv"]');
-		const downloadHref = await downloadButton.getAttribute("href");
-		expect(downloadHref).toBe(`${EXPECTED_DOWNLOAD_URL}`);
-	});
-
-	test("should display table", async ({ page }) => {
-		const jobsTable = await page.locator('[data-testid="jobs-table"]');
-		await expect(jobsTable).toBeVisible();
+	test("should display try again button", async ({ page }) => {
+		const tryAgainButton = await page.getByRole("button", { name: "Try again" });
+		await expect(tryAgainButton).toBeVisible();
 	});
 });
