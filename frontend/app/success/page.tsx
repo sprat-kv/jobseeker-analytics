125b1e5fd0db680d6bf45dad9e8ebfb85bdc862d
Jed Antalan <jediel2424@gmail.com>
2025-03-07 20:55:10 -0500

    change success page to pass needed cookies to backend for download

--- frontend/app/success/page.tsx
+++ frontend/app/success/page.tsx
@@ -1,16 +1,53 @@
+"use client";
+import { useState } from "react";
+import { Button } from "@heroui/react";
+
 export default function SuccessPage() {
 	const apiUrl = process.env.NEXT_PUBLIC_API_URL!;
+	const [isDownloading, setIsDownloading] = useState(false);
+
+	const handleDownload = async () => {
+		setIsDownloading(true);
+
+		try {
+			const response = await fetch(`${apiUrl}/download-file`, {
+				method: "GET",
+				credentials: "include"
+			});
+
+			if (!response.ok) {
+				if (response.status === 404) {
+					throw new Error("File not found");
+				} else {
+					throw new Error("Failed to download file");
+				}
+			}
+
+			// Download file
+			const blob = await response.blob();
+			const url = window.URL.createObjectURL(blob);
+			const a = document.createElement("a");
+			a.href = url;
+			a.download = "job_applications.csv";
+			document.body.appendChild(a);
+			a.click();
+			window.URL.revokeObjectURL(url);
+			document.body.removeChild(a);
+		} catch (error) {
+			error;
+		} finally {
+			setIsDownloading(false);
+		}
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center text-center pt-64">
 			<h1 className="text-3xl font-bold text-green-500">Success! Your file is ready.</h1>
 			<p className="pt-8">Click the button below to download your file.</p>
-			<a
-				download
-				className="mt-6 px-6 py-3 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 transition"
-				href={`${apiUrl}/download-file`}
-			>
-				Download Job Application Data
-			</a>
+
+			<Button className="mt-4" color="success" isDisabled={isDownloading} size="lg" onPress={handleDownload}>
+				{isDownloading ? "Downloading..." : "Download Job Application Data"}
+			</Button>
 		</div>
 	);
 }
