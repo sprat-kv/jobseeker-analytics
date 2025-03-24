eb53f5d1e5ac1dc52b2c5549d07040df81b195e0
happynasit <105992603+happynasit@users.noreply.github.com>
2025-03-24 12:12:59 -0400

    Update page.tsx

--- frontend/app/response-rates/page.tsx
+++ frontend/app/response-rates/page.tsx
@@ -1,6 +1,6 @@
 import UniqueOpenRateChart from '@/components/response_rate_chart';
 
-export default function Home() {
+export default function ResponseRates() {
   return (
     <main className="p-8">
       <UniqueOpenRateChart />
