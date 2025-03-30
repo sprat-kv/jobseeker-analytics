3636c49533b9536e0ba97cc6fa905fb47031da02
happynasit <105992603+happynasit@users.noreply.github.com>
2025-03-30 00:06:10 -0400

    Update response_rate_chart.tsx

--- frontend/components/response_rate_chart.tsx
+++ frontend/components/response_rate_chart.tsx
@@ -64,7 +64,7 @@ export default function JobTitleResponseChart() {
 				console.error("Failed to load data");
 			} finally {
 				// Set loading to false
-				setLoading(false);
+				
 			}
 		};
 
