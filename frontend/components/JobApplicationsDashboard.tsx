24a21860b665329511ccfa28ff21d62483e2af95
hrishav398 <hrishav398@gmail.com>
2025-06-09 00:12:40 -0400

    turn yellow

--- frontend/components/JobApplicationsDashboard.tsx
+++ frontend/components/JobApplicationsDashboard.tsx
@@ -330,7 +330,7 @@ export default function JobApplicationsDashboard({
 												${
 												item.application_status.toLowerCase() === "Rejection"
 													? "bg-red-100 text-red-800"
-													: "bg-red-100 text-green-800"
+													: "bg-yellow-100 text-green-800"
 											}`
 										}
 										>
