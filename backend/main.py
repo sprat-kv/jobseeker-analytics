7b22fe1f46306afa0e9de06f8975dd938a4e9f25
Lianna Novitz <liannanovitz@gmail.com>
2025-07-25 17:11:02 -0700

    add frontend and api origin

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ app.add_middleware(SlowAPIMiddleware)
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.APP_URL],  # Allow frontend origins
+    allow_origins=[settings.APP_URL, settings.API_URL],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
