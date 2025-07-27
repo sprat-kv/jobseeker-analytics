153011b50db928bda1a7dbeb0ebfd2b0acfcda8a
Lianna Novitz <liannanovitz@gmail.com>
2025-07-27 11:45:56 -0700

    more logging

--- backend/utils/cookie_utils.py
+++ backend/utils/cookie_utils.py
@@ -1,6 +1,9 @@
 from fastapi import Response
 from utils.config_utils import get_settings
+import logging
 
+# Logger setup
+logger = logging.getLogger(__name__)
 settings = get_settings()
 
 
@@ -35,6 +38,13 @@ def set_conditional_cookie(
             cookie_params["key"] = f"__Host-{cookie_params['key']}"
         else:
             cookie_params["key"] = f"__Secure-{cookie_params['key']}"
+
+    # Debug logging
+    logging.info(f"Setting cookie with parameters: {cookie_params}")
+    logging.info(f"settings.ORIGIN: {settings.ORIGIN}")
+    logging.info(f"settings.is_publicly_deployed: {settings.is_publicly_deployed}")
+    logging.info(f"settings.APP_URL: {settings.APP_URL}")
+    logging.info(f"settings.API_URL: {settings.API_URL}")
 
     response.set_cookie(**cookie_params)
     return response
